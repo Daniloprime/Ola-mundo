8b6c9f19fdc005f36f564213eb7b15cc52149cca
Danilo Ferreira Pereira <164246261+Daniloprime@users.noreply.github.com>
2024-03-21 22:11:58 -0300

    quase pronto

---
+++ site de calculadora/script.js
@@ -0,0 +1,45 @@
+
+function calcular(){
+    var n1 = document.querySelector('#txt1') 
+    var n2 = document.querySelector('#txt2')
+
+    var somar = Number(n1.value) + Number(n2.value)
+
+    res.innerHTML = `o Resultado é ${somar}`
+}
+
+function calcular(){
+    var n1 = document.querySelector('#txt1') 
+    var n2 = document.querySelector('#txt2')
+
+    var subtrair = Number(n1.value) - Number(n2.value)
+
+    res.innerHTML = `o Resultado é ${subtrair}`
+}
+
+function calcular(){
+    var n1 = document.querySelector('#txt1') 
+    var n2 = document.querySelector('#txt2')
+
+    var dividir = Number(n1.value) / Number(n2.value)
+
+    res.innerHTML = `o Resultado é ${dividir}`
+}
+
+function calcular(){
+    var n1 = document.querySelector('#txt1') 
+    var n2 = document.querySelector('#txt2')
+
+    var multi = Number(n1.value) * Number(n2.value)
+
+    res.innerHTML = `o Resultado é ${multi}`
+}
+
+function calcular(){
+    var n3 = document.querySelector('#txt3') 
+    var n4 = document.querySelector('#txt4')
+
+    var media = (Number(n3.value) + Number(n4.value)) / 2
+
+    res.innerHTML = `o Resultado é ${media}`
+}
